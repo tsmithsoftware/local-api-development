4c6a19999d1ef4c3c6b4343d2dcf89f958e98f1d
tsmithsoftware <tijak@protonmail.ch>
2022-09-07 22:38:01 +0100

    add post to serverless file, modify post function to parse body correctly

--- lambda_funcs/postUser/index.js
+++ lambda_funcs/postUser/index.js
@@ -2,7 +2,7 @@ const { User, getSequelizeClient } = require('../../utils/database_connection');
 const client = getSequelizeClient();
 
 exports.handler = async (event) => {
-	const parsedUser = JSON.parse(event);
-	const user = await User.create({lastName: event.lastName, firstName: event.firstName});
-	return {};
+	const parsedEvent = JSON.parse(event.body);
+	const user = await User.create({lastName: parsedEvent.lastName, firstName: parsedEvent.firstName});
+	return JSON.stringify({});
 };
