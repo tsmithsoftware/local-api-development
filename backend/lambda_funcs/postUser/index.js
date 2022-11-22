ed9e4b97df8bcc7407500260721970a1e3939f75
Timothy Smith <timothy.smith1@bp.com>
2022-11-22 14:59:35 +0000

    fix postUser function

--- backend/lambda_funcs/postUser/index.js
+++ backend/lambda_funcs/postUser/index.js
@@ -1,15 +1,25 @@
-const { User } = require('../../utils/database_connection');
+const { StatusCodes } = require('http-status-codes');
+const { response, errorResponse } = require("../../utils/response");
+const { getSequelizeClient } = require('../../utils/database_connection');
+const { Sequelize, DataTypes } = require('sequelize');
+const UserModel = require('../../models/user')
 
 exports.handler = async (event, context) => {
-	const parsedEvent = JSON.parse(event.body);
-	await User.create({lastName: parsedEvent.lastName, firstName: parsedEvent.firstName})
-		.then(
-			user => {
-				return response(context, StatusCodes.CREATED,  JSON.stringify({ lastName: user.lastName, firstName: user.firstName }))
-			}
-		)
-		.catch(error => {
-			return errorResponse(context, StatusCodes.INTERNAL_SERVER_ERROR, error)
+  await getSequelizeClient
+    .then(async (client) => {
+		const parsedEvent = JSON.parse(event.body)
+		let User = UserModel(client, Sequelize)
+		await User.create(
+		{
+			lastName: parsedEvent.lastName,
+			firstName: parsedEvent.firstName
+		}) 
+		.then( user => {
+			return response(context, StatusCodes.CREATED, JSON.stringify( {lastName: user.lastName, firstName: user.firstName} ))
 		}
 		)
-};
+		.catch(error => {
+		  return errorResponse(context, StatusCodes.INTERNAL_SERVER_ERROR, error)
+		})
+    })
+}
