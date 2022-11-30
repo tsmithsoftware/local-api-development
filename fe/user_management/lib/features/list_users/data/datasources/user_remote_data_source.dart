da901503309170dd299a4f0f059c50b31a38192c
Timothy Smith <timothy.smith1@bp.com>
2022-11-30 15:03:19 +0000

    fix analyser issues for dependencies

--- fe/user_management/lib/features/list_users/data/datasources/user_remote_data_source.dart
+++ fe/user_management/lib/features/list_users/data/datasources/user_remote_data_source.dart
@@ -1,9 +1,22 @@
 
 import '../models/user_list_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class UserRemoteDataSource {
   /// Calls the getUsers endpoint to retrieve all users in the system
   ///
   /// Throws a [ServerException] for all error codes
   Future<UserListModel> getUsers();
+}
+
+class UserRemoteDataSourceImpl extends UserRemoteDataSource {
+  final http.Client client;
+
+  UserRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<UserListModel> getUsers() {
+    // TODO: implement getUsers
+    throw UnimplementedError();
+  }
 }
