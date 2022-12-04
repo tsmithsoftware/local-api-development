895ef32e0538052963297631d56e9027b546f72e
Timothy Smith <timothy.smith1@bp.com>
2022-12-04 17:32:19 +0000

    added babel, jest, test for /root endpoint lambda

--- backend/lambda_funcs/test/root.spec.js
+++ backend/lambda_funcs/test/root.spec.js
@@ -1,1 +1,11 @@
-import root from '../root'
+import lambdaTester from 'lambda-tester';
+import { handler } from '../root'
+
+test('root returns 204 code if available', async () => {
+    const event = {};
+    await lambdaTester(handler)
+        .event(event)
+        .expectResult( (result) => {
+            expect(result.statusCode).toEqual(204);
+        } );
+});
