d175c40dbee32121353ef3fa0e26f0b666cc0f46
Timothy Smith <timothy.smith1@bp.com>
2022-12-08 14:43:00 +0000

    add all possible connection setups

--- backend/lambda_funcs/sequelizeInstance.js
+++ backend/lambda_funcs/sequelizeInstance.js
@@ -13,8 +13,7 @@ let sequelizeInstance = new Sequelize('db', 'user', 'password', {
 
 sequelizeInstance.authenticate().then(() => {
   console.log('Connection to database has been established successfully.')
-}).catch(err => {
-  console.error('Unable to connect to database:', err)
+}).catch(_ => {
   sequelizeInstance = new Sequelize('db', 'user', 'password', {
     host: 'host.docker.internal',
     dialect: 'mysql',
@@ -26,6 +25,20 @@ sequelizeInstance.authenticate().then(() => {
       acquire: 30000
     }
   })
+}).catch(_ => {
+  sequelizeInstance = new Sequelize('db', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+      max: 15,
+      min: 5,
+      idle: 20000,
+      evict: 15000,
+      acquire: 30000
+    }
+  })
+}).catch(_ => {
+  console.error('Connection cannot be established.')
 })
 
 const db = {}
