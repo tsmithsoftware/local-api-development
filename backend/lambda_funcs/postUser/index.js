5646b07dbf4d8e6819294c74795488a332e7e2a3
Timothy Smith <timothy.smith1@bp.com>
2022-12-08 14:28:00 +0000

    add userADO to post user, fix integration tests

--- backend/lambda_funcs/postUser/index.js
+++ backend/lambda_funcs/postUser/index.js
@@ -1,27 +1,13 @@
 const { StatusCodes } = require('http-status-codes')
 const { response, errorResponse } = require('../../utils/response')
-const ConnectionService = require('../../utils/connection_service')
-const UserModel = require('../../models/user')
-const Sequelize = require('sequelize')
 
 exports.handler = async (event, context) => {
-  const connectionService = new ConnectionService()
-  await connectionService.getSequelizeClient()
-    .then(async (client) => {
-      const parsedEvent = JSON.parse(event.body)
-      const User = UserModel(client, Sequelize)
-      await User.create(
-        {
-          uuid: parsedEvent.uuid,
-          lastName: parsedEvent.lastName,
-          firstName: parsedEvent.firstName
-        })
-        .then(user => {
-          return response(context, StatusCodes.CREATED, JSON.stringify({ uuid: user.uuid, lastName: user.lastName, firstName: user.firstName }))
-        }
-        )
-        .catch(error => {
-          return errorResponse(context, StatusCodes.INTERNAL_SERVER_ERROR, error)
-        })
+  const UserDAO = require('../daos/userDAO')
+  await UserDAO.postUser(event)
+    .then((user) => {
+      // eslint-disable-next-line object-shorthand
+      return response(context, StatusCodes.CREATED, JSON.stringify({ uuid: user.uuid, lastName: user.lastName, firstName: user.firstName }))
+    }).catch(error => {
+      return errorResponse(context, StatusCodes.INTERNAL_SERVER_ERROR, error)
     })
 }
